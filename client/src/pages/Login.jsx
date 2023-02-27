c67f1d7ebb5896eae09b7dd3797f9333b4d0ab3c
TuDoan97 <105418115+TuDoan97@users.noreply.github.com>
2023-02-27 09:14:37 +0700

    Tạo AuthContext lưu trữ thông tin user

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Typography } from "@mui/material";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { AuthContext } from "../context/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const auth = getAuth();
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLoginWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+
+    const res = await signInWithPopup(auth, provider);
+    console.log({ res });
+  };
+
+  if (user?.uid) {
+    navigate("/");
+    return;
+  }
   return (
     <>
       <Typography variant="h5" sx={{ marginBottom: "10px" }}>
         Welcome to Note App
       </Typography>
-      <Button variant="outlined">Login with Google</Button>
+      <Button variant="outlined" onClick={handleLoginWithGoogle}>
+        Login with Google
+      </Button>
     </>
   );
 }
--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,22 +1,35 @@
 import { createBrowserRouter, Outlet } from "react-router-dom";
+import AuthProvider from "../context/AuthProvider";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import ErrorPage from "./ErrorPage";
+import ProtectedRoute from "./ProtectectedRoute";
 
 const AuthLayOut = () => {
-  return <Outlet />;
+  return (
+    <AuthProvider>
+      <Outlet />
+    </AuthProvider>
+  );
 };
 
 export default createBrowserRouter([
   {
-    element: <AuthLayOut/>,
+    element: <AuthLayOut />,
+    errorElement: <ErrorPage />,
     children: [
       {
         element: <Login />,
         path: "/login",
       },
       {
-        element: <Home />,
-        path: "/",
+        element: <ProtectedRoute />,
+        children: [
+          {
+            element: <Home />,
+            path: "/",
+          },
+        ],
       },
     ],
   },
