e05b0a2fe03c5172289f1dad28947ea5b2734555
TuDoan97 <105418115+TuDoan97@users.noreply.github.com>
2023-03-01 08:50:53 +0700

    xử lý accessToken FrontEnd

--- client/src/utils/folderUtils.js
+++ client/src/utils/folderUtils.js
@@ -1,3 +1,5 @@
+import { graphQLRequest } from "./request";
+
 export const foldersLoader = async () => {
   const query = `query ExampleQuery {
       folders {
@@ -7,18 +9,6 @@ export const foldersLoader = async () => {
       }
     }`;
 
-  const res = await fetch("http://localhost:4000/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      query,
-    }),
-  });
-
-  const { data } = await res.json();
-  console.log({ data });
+  const data = await graphQLRequest({ query });
   return data;
 };
